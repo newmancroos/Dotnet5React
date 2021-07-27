5f47b42003fb28738c1c40e47bd6d3d3f7d63cd3
newmancroos <newmancroos@gmail.com>
2021-07-27 13:59:39 -0400

    Completed - can handle auth token using cookie

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Dotnet5React.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new CreateReactAppViewModel(HttpContext);
+
+            return View(vm);
+        }
+        public async Task<ActionResult> Login()
+        {
+            var userId = Guid.NewGuid().ToString();
+            var claims = new List<Claim>
+              {
+                new(ClaimTypes.Name, userId)
+              };
+
+            var claimsIdentity = new ClaimsIdentity(
+              claims,
+              CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(
+              CookieAuthenticationDefaults.AuthenticationScheme,
+              new ClaimsPrincipal(claimsIdentity),
+              authProperties);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(
+              CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
@@ -19,7 +20,12 @@ namespace Dotnet5React
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                });
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
@@ -45,12 +51,21 @@ namespace Dotnet5React
             app.UseSpaStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                  "default",
+                  "{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute(
+                  "counter",
+                  "/counter",
+                  new { controller = "Home", action = "Index" });
+                endpoints.MapControllerRoute(
+                  "fetch-data",
+                  "/fetch-data",
+                  new { controller = "Home", action = "Index" });
             });
 
             app.UseSpa(spa =>
